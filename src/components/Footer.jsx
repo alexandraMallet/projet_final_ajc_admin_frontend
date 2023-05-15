ee14279042eddd47942b663c7328a2b5abd52003
Alexandra Mallet <malletalexandra@gmail.com>
2023-05-15 15:15:44 +0200

    création arborescence + header + footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+function Footer(){
+
+    return (
+        <>
+        
+        <div className="container p-5 my-5 bg-success text-white">
+
+        <div className="contact">Hotline : help@clementine.com</div>
+
+
+        </div>
+
+        
+        </>
+    )
+}
+
+export default Footer
---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+function Header () {
+
+    return (
+
+        <>
+        
+        <div className="container p-5 my-5 bg-success text-white">
+
+            <h1>Clémentine - administration du site</h1>
+
+        </div>
+        
+        </>
+    )
+}
+
+export default Header
